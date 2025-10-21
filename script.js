0d3e1fe6d3c720e9718015b02e9d683f6e2514d6
CherryRuby117 <guerreroalvinezalexarubi@gmail.com>
2025-10-20 22:04:33 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const lightbox = document.getElementById('lightbox');
+const imagenAmpliada =
+document.getElementById('imagen-ampliada');
+const caption = document.getElementById('caption');
+const cerrar = document.querySelector('.cerrar');
+const imagenes = document.querySelectorAll('.galeria img');
+
+imagenes.forEach(img => {
+	img.addEventListener('click',()=> {
+		lightbox.style.display ='block';
+		imagenAmpliada.src = img.src;
+		caption.textContent = img.alt;
+	});
+});
+
+cerrar.addEventListener('click',() => {
+	lightbox.style.display ='none';
+});
